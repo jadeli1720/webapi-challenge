959b7748828bc3c219dfaa5a6294432b88afdf40
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-09-06 10:27:58 -0600

    Made post a project and used middleware to validate project and last commit project by id

--- projectsRouter.js
+++ projectsRouter.js
@@ -19,30 +19,38 @@ router.get('/', (req, res) => {
 });
 
 //This grabbed the project of specified id and all of it's actions/
-router.get('/:id', validateUserId, (req,res) => {
+router.get('/:id', validateProjectId, (req, res) => {
     res.status(200).json(req.project)
 
 });
 
 //do we need?
-router.get('/:id/actions', (req,res) => {
+router.get('/:id/actions', (req, res) => {
 
 });
 
 
 /************************* POST **************************/
 
-router.post('/', (req,res) => {
+router.post('/', validatProject, (req, res) => {
+    const project = req.body;
 
+    Project.insert(project)
+        .then(project => {
+            res.status(201).json(project)
+        })
+        .catch(err => {
+            res.status(500).json({ error: "There was an error while saving the project to the database" })
+        })
 });
 
-router.post('/', (req,res) => {
+router.post('/', (req, res) => {
 
 });
 
 /************************* Delete **************************/
 
-router.delete('/', (req,res) => {
+router.delete('/', (req, res) => {
 
 });
 
@@ -56,7 +64,7 @@ router.delete('/', (req,res) => {
 //custom middleware
 
 //validates user id on all endpoints using id parameters
-function validateUserId(req, res, next) {
+function validateProjectId(req, res, next) {
     const { id } = req.params
     Project.get(id)
         .then(project => {
@@ -73,6 +81,19 @@ function validateUserId(req, res, next) {
         })
 
 };
+
+function validatProject(req,res,next) {
+    const { name, description } = req.body;
+
+    if(!name || !description){
+        return res.status(400).json({errorMessage: "A name and a description is required"})
+    }
+    if (typeof name !== "string" || typeof description !== 'string'){
+        return res.status(400).json({errorMessage: "The name and description must be strings"})
+    }
+    req.body = { name, description };
+    next()
+}
 
 
 module.exports = router;

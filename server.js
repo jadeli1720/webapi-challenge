b1fd420e1d6da7236d2801e1f242793766c7349b
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-09-06 09:50:31 -0600

    Set up actions and project router files with export/imports to server.js

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const projectsRouter = require('./projectsRouter');
+const actionsRouter = require('./actionsRouter');
 
 console.log('environment', process.env.NODE_ENV);
 
@@ -8,5 +10,9 @@ const server = express();
 server.get('/', (req, res) => {
     res.send(`<h2>It's Working! It's Working!</h2>`)
   });
+
+  server.use('/projects', projectsRouter );
+  server.use('/actions', actionsRouter );
+
 
 module.exports = server;
